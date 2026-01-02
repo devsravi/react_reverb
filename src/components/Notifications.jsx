0172546ab46a277a53f117566edd5735375dea03
Ravi-FLA705 <ravi.kumar1@lyxelandflamingo.com>
2026-01-02 12:23:41 +0530

    Add repo for reverb notification test public channel

---
+++ src/components/Notifications.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import echo from "../lib/echo";
+
+export default function Notifications() {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    echo.channel("notifications")
+      .listen(".notification.sent", (e) => {
+         console.log("EVENT RECEIVED", e);
+        setNotifications((prev) => [e.notification, ...prev]);
+      });
+
+    return () => {
+      echo.leave("notifications");
+    };
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Real-Time Notifications</h2>
+
+      {notifications.length === 0 && (
+        <p>No notifications yet</p>
+      )}
+
+      {notifications.map((n, index) => (
+        <div
+          key={index}
+          style={{
+            border: "1px solid #ddd",
+            padding: "10px",
+            marginBottom: "10px",
+            borderRadius: "6px",
+          }}
+        >
+          <strong>{n.title}</strong>
+          <p>{n.message}</p>
+          <small>{n.time}</small>
+        </div>
+      ))}
+    </div>
+  );
+}
